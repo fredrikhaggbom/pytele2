20ae5e675a02cd8086ffa4c240a4d1db66b3b083
Fredrik Häggbom <fredrik.haggbom@dizparc.se>
2023-06-01 09:23:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytele2api",
+    version="1.0.0",
+    author="Fredrik Häggbom",
+    author_email="fredrik.haggbom@gmail.com",
+    description="Python library for communication with Tele2 My TSO",
+    license="Apache 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fredrikhaggbom/pytele2",
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "datetime"],
+    tests_require=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
+)
